16b07ccbc281ed45dfe71778791d81dc62e8828c
Ali_karim_e <Ali_karim_e@outlook.com>
2023-04-23 10:30:26 +0430

    this commit static file is load but dont understand!!!

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'account'
 urlpatterns = [
@@ -7,4 +9,4 @@ urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='user_login'),
     path('logout/', views.UserLogoutView.as_view(), name='user_logout'),
     path('profile/<int:user_id>/',views.UserProfileView.as_view(), name='user_profile'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
